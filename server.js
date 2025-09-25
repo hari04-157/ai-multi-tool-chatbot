acb5c274f00d7fcfdea3346224a8b9c16c9d12f5
hari04-157 <hari04157157.com>
2025-09-25 16:07:17 +0530

    updated

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// --- **UPDATED Session Configuration for Cross-Site Cookies** ---
+// --- Session Configuration for Cross-Site Cookies ---
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
